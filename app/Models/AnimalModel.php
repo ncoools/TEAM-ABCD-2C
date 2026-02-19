9a43b0912057f1b29f60e6aaac9f61a756b50907
ncoools <rendonicolw@gmailcom>
2026-02-19 14:01:45 +0800

    Add AnimalModel to Models folder

---
+++ app/Models/AnimalModel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class AnimalModel extends Model
+{
+    protected $table = 'animals';
+    protected $primaryKey = 'id';
+
+    protected $allowedFields = ['name', 'classes'];
+
+    public function getRecords($start, $length, $searchValue = '')
+    {
+        $builder = $this->builder();
+        $builder->select('*');
+
+        if (!empty($searchValue)) {
+            $builder->groupStart()
+                ->orLike('name', $searchValue)
+                ->groupEnd();
+        }
+
+        // Clone builder for filtered count before applying limit
+        $filteredBuilder = clone $builder;
+        $filteredRecords = $filteredBuilder->countAllResults();
+
+        $builder->limit($length, $start);
+        $data = $builder->get()->getResultArray();
+
+        return ['data' => $data, 'filtered' => $filteredRecords];
+    }
+}
