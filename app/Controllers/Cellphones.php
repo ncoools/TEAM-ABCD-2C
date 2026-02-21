78b94df34a4df698f6a6c53f42ef9033e6049160
ncoools <rendonicolw@gmailcom>
2026-02-22 00:05:40 +0800

    VILLAESTIVA - UPDATE

--- app/Controllers/Cellphones.php
+++ app/Controllers/Cellphones.php
@@ -74,56 +74,7 @@ class Cellphones extends Controller
         $model = new CellphonesModel();
     $user = $model->find($id); // Fetch user by ID
 
-    if ($user) {
-        return $this->response->setJSON(['data' => $user]); // Return user data as JSON
-    } else {
-        return $this->response->setStatusCode(404)->setJSON(['error' => 'User not found']);
-    }
-}
-
-public function delete($id){
-    $model = new CellphonesModel();
-    $logModel = new LogModel();
-    $user = $model->find($id);
-    if (!$user) {
-        return $this->response->setJSON(['success' => false, 'message' => 'User not found.']);
-    }
-
-    $deleted = $model->delete($id);
-
-    if ($deleted) {
-        $logModel->addLog('Delete user', 'DELETED');
-        return $this->response->setJSON(['success' => true, 'message' => 'User deleted successfully.']);
-    } else {
-        return $this->response->setJSON(['success' => false, 'message' => 'Failed to delete user.']);
-    }
-}
-
-public function fetchRecords()
-{
-    $request = service('request');
-    $model = new \App\Models\CellphonesModel();
-
-    $start = $request->getPost('start') ?? 0;
-    $length = $request->getPost('length') ?? 10;
-    $searchValue = $request->getPost('search')['value'] ?? '';
-
-    $totalRecords = $model->countAll();
-    $result = $model->getRecords($start, $length, $searchValue);
-
-    $data = [];
-    $counter = $start + 1;
-    foreach ($result['data'] as $row) {
-        $row['row_number'] = $counter++;
-        $data[] = $row;
-    }
-
-    return $this->response->setJSON([
-        'draw' => intval($request->getPost('draw')),
-        'recordsTotal' => $totalRecords,
-        'recordsFiltered' => $result['filtered'],
-        'data' => $data,
-    ]);
+   
 }
 
 }
